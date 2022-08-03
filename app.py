dbf3a80f5990fed20683ad63b67c80640fd0942c
Varun <varunreddy.ponnapureddy@my.utexas.edu>
2022-08-02 22:41:15 -0500

    Flask React Heroku

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask;
+from flask import Flask, send_from_directory;
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__, static_folder='./my-app/build', static_url_path='/')
+CORS(app)
 
 @app.route('/')
 def index():
